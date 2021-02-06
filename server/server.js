9b55e9742a9379e060482252050d244753f44273
geigea84 <geigea84@gmail.com>
2021-02-05 23:16:29 -0600

    set up server.js, created schemas and schema files

--- server/server.js
+++ server/server.js
@@ -3,13 +3,31 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-//add apollo server
+//import ApolloServer
 const {ApolloServer} = require('apollo-server-express');
+
+//import authorization middleware
+const {authMiddleware} = require('./utils/auth');
+
+//import typeDefs and resolvers
+const {typeDefs, resolvers} = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//implement ApolloServer
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+})
+
+//apply ApolloServer to Express server as middleware
+server.applyMiddleware({app});
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
