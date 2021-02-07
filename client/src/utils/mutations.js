93dfa88f0e2c9dcb019547ca19e54b61d74eeee0
geigea84 <geigea84@gmail.com>
2021-02-06 19:58:10 -0600

    finished front end queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import gql from 'graphql-tag';
+
+//see typeDefs for mutations
+//remember Book model runs through User model
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookInputData: BookInput!) {
+        saveBook(bookInputData: $bookInputData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+
+//_id
+//username
+//email
+//bookCount
+//savedBooks
+////savedBooks data
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
